68b9adffd1cd4afb6bbcebdc5f67e835d7219a48
felipe0612 <Felipe.bp1210@gmail.com>
2024-02-01 15:40:15 -0300

    header

---
+++ BalleCoffe (Alunos)/BalleCoffe (Alunos)/efeitos.js
@@ -0,0 +1,30 @@
+// Efeito (Direita p/ esquerda) do container principal
+
+// Seleciona o elemento com a classe 'container'
+const container = document.querySelector('.container');
+
+// Adiciona uma classe 'slide-from-right' ao elemento quando a página é carregada
+window.addEventListener('load', function() {
+    container.classList.add('slide-from-right');
+});
+
+// ________________________________________________________________________________
+// Responsividade (Header)
+
+// Função JS para toggle menu 
+  
+  // Obtém a referência do elemento com o ID "MenuItems"
+  var MenuItems = document.getElementById("MenuItems");
+
+  // Define a altura máxima do estilo do elemento MenuItems como "0px"
+  MenuItems.style.maxHeight = "0px";
+
+  // Define uma função chamada menutoggle
+  function menutoggle() {
+    // Verifica se a altura máxima do estilo do elemento MenuItems é "0px"
+    if (MenuItems.style.maxHeight == "0px") {
+    } else {
+      // Caso contrário, se não for "0px", define a altura máxima do estilo do elemento MenuItems como "0px"
+      MenuItems.style.maxHeight = "0px";
+    }
+  }
